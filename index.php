35a7b16c6ce644aaf464b4caacc99109d3f9acb2
formk <formk@kermed.net>
2022-06-29 10:30:52 +0200

    Ajout d'une fonction debug

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <?php
 include('includes/header.php');
+include('includes/fonctions.php');
 ?>
 <main>
+<?php
+// $tableau = array();
+$tableau =  [];
+$tableau = [true,45,'Terre',[45,46,47]];
 
+debug($tableau);
+
+
+?>
 </main>
 <?php
 include('includes/footer.php');
